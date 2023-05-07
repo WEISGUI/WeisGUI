aa96dc8243cdbc9fdf9b71f250e0e4e3c359f366
B <bdeguz1@students.towson.edu>
2023-05-07 01:53:13 -0400

    Added some more button functionality

--- src/SuppliersPage.java
+++ src/SuppliersPage.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.SQLException;
 
 public class SuppliersPage extends JDialog {
     private JPanel SuppliersPagePanel;
@@ -31,6 +34,71 @@ public class SuppliersPage extends JDialog {
         setMinimumSize(new Dimension(1535,850));
         setModal(true);
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+
+        homeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                HomePage homePage = new HomePage(null);
+            }
+        });
+
+
+        categoriesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                try {
+                    CategoryPages categoryPages = new CategoryPages(null);
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
+            }
+        });
+
+        productLocationButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                ProductsLocationPage productsLocationPage = new ProductsLocationPage(null);
+            }
+        });
+
+        suppliersButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                SuppliersPage suppliersPage = new SuppliersPage(null);
+            }
+        });
+
+
+        shipmentsButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                ShipmentPage shipmentPage = new ShipmentPage(null);
+            }
+        });
+
+
+        inventoryButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+
+        accountButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                AccountPage accountPage = new AccountPage(weisEmployee);
+            }
+        });
+
         setVisible(true);
     }
 
